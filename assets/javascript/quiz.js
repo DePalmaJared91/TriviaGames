63247585bf779cd2bd0697ca2341ed8ab7ae44e5
Jared DePalma <depalmajared91@gmail.com>
2020-03-04 14:41:21 -0700

    More commits with a review of the quiz.js. I think I fixed some problems. Next step emoji pics.

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -18,7 +18,7 @@ let questions = [
 		choiceA: "1770",
 		choiceB: "1778",
 		choiceC: "1701",
-		correct: "C",
+		correct: "C"
 	},
 	{
 		question: "The Yale Daily News is the country's oldest collegiate newspaper which has been in print since. . . ?",
@@ -26,160 +26,92 @@ let questions = [
 		choiceA: "1922",
 		choiceB: "1791",
 		choiceC: "1878",
-		correct: "C",
-	},
-	{
-		question: "What was Yale University's name between 1701 and 1716?",
-		imgSrc: "images/collegaiteschool.jpeg",
-		choiceA: "The Ivy School",
-		choiceB: "The British East India School",
-		choiceC: "The  Collegiate School",
-		correct: "C",
-	},
-	{
-		question: "What was Yale University's name between 1701 and 1716?",
-		imgSrc: "images/collegaiteschool.jpeg",
-		choiceA: "The Ivy School",
-		choiceB: "The British East India School",
-		choiceC: "The  Collegiate School",
-		correct: "C",
-		},
-			
-		{
-		question: "What was Yale University's name between 1701 and 1716?",
-		imgSrc: "images/collegaiteschool.jpeg",
-		choiceA: "The Ivy School",
-		choiceB: "The British East India School",
-		choiceC: "The  Collegiate School",
-		correct: "C",
-	},
-	{
-	question: "What was Yale University's name between 1701 and 1716?",
-	imgSrc: "images/collegaiteschool.jpeg",
-	choiceA: "The Ivy School",
-	choiceB: "The British East India School",
-	choiceC: "The  Collegiate School",
-	correct: "C",
-	},
+		correct: "C"
+	}];
 
-	{
-	question: "What was Yale University's name between 1701 and 1716?",
-	imgSrc: "images/collegaiteschool.jpeg",
-	choiceA: "The Ivy School",
-	choiceB: "The British East India School",
-	choiceC: "The  Collegiate School",
-	correct: "C",
-	},
-	
-	{
-		question: "What was Yale University's name between 1701 and 1716?",
-		imgSrc: "images/collegaiteschool.jpeg",
-		choiceA: "The Ivy School",
-		choiceB: "The British East India School",
-		choiceC: "The  Collegiate School",
-		correct: "C",
-	},
-	
-	];
+//:variables
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; //10s
+const gaugeWidth = 150;//150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
 
-	//:variables
-	const lastQuestion = questions.length - 1;
-	let runningQuestion = 0;
-	let count = 0;
-	const questionTime =10; //10s
-	const gaugeWidth = 150;//150px
-	const gaugeUnit = gaugeWidth / questionTime;
-	let Timer; 
-	let score = 0;
-
-	//render a question
-	function renderQuestion(){
-let q = questions[runningQuestion];
-
-question.innerHTML = "<p>"+ q.question +"</p>";
-qImg.innerHTML ="<img src"+ q.imgSrc + imgSrc +">";
-choiceA.innerHTML = q.choiceA;
-choiceB.innerHTML= q.choiceB;
-choiceC.innerHTML = q.choiceC;
-/////PICK UP HERE
-}
-
-
-let lastQuestionIndex = questions.length - 1;
-let runningQuestionIndex = 0;
+//render a question
 function renderQuestion() {
+	let q = questions[runningQuestion];
 
-	let q = questions[runningQuestionIndex];
-	qImg.innerHTML = "<img src=" + q.imgSrc + ">";
 	question.innerHTML = "<p>" + q.question + "</p>";
+	qImg.innerHTML = "<img src" + q.imgSrc + ">";
 	choiceA.innerHTML = q.choiceA;
-	choiceB.innerHTMl = q.choiceB;
+	choiceB.innerHTML = q.choiceB;
 	choiceC.innerHTML = q.choiceC;
 }
-
-
-
 start.addEventListener("click", startQuiz);
-//Start Quiz
-function startQuiz(){
-start.style.display= "none";
-renderQuestion();
-quiz.style.display = "block"
-renderProgress();
-renderCounter();
-TIMER = setInterval(renderCounter,1000);
+//start quiz
+function startQuiz() {
+
+	start.style.display = "none";
+	renderQuestion();
+	quiz.style.display = "block";
+	renderProgress();
+	renderCounter();
+	TIMER = setInterval(renderCounter, 1000);
 }
-
 //render progress
-function renderProgress(){
-	for(let qIndex = 0 ; qIndex<= lastQuestion; qIndex++){
+function renderProgress() {
+	for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
 
-		progress.innerHTML += "<div class='prog' id="+qIndex+"></div>";
+		progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
 
 	}
 }
 
 //counter render
 
-function renderCounter(){
-	if(count <= questionTime){
+function renderCounter() {
+	if (count <= questionTime) {
 		counter.innerHTML = count;
 		timeGauge.style.width = count * gaugeUnit + "px";
 		count++
 
-	}else{
-		count= 0
+	} else {
+		count = 0
 		//change progress color to red
 		answerIsWrong();
-		if(runningQuestion < lastQuestion){
+		if (runningQuestion < lastQuestion) {
 			runningQuestion++;
 			renderQuestion();
 
-		}else{//end quiz show score
+		} else {//end quiz show score
 			clearInterval(TIMER);
 			scoreRender();
 
 		}
 	}
 }
+//pick up here
+
 
 //check answer
-function checkAnswer(answer{
-	if( answer == questions[runningQuestion].correct){
+function checkAnswer(answer){
+	if(answer == questions[runningQuestion].correct){
 	//answer is correct	
-		score++;
-// change progress color to red
-answerIsCorrect();
-}else{
+	score++;
+	// change progress color to red
+	answerIsCorrect();
+}else {
 	//answer is wrong
-	//change progress colo to red
+	//change progress color to red
 	answerIsWrong();
 }
-count=0
-if (runningQuestion < lastQuestion){
+count = 0
+if (runningQuestion < lastQuestion) {
 	runningQuestion++;
 	renderQuestion();
-}else{
+} else {
 	//end quiz show score
 	clearInterval(TIMER);
 	scoreRender();
@@ -187,36 +119,36 @@ if (runningQuestion < lastQuestion){
 }}
 
 //answer is correct
- function answerIsCorrect(){
-	 document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-	}
-	//answer is wrong
-	function answerIswrong(){
-		document.getElementById(runningQuestion).style.backgroundColor = "f00";
+function answerIsCorrect() {
+	document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+//answer is wrong
+function answerIswrong() {
+	document.getElementById(runningQuestion).style.backgroundColor = "f00";
 
-	}
-	//score render
-	function scoreRender(){
-		scoreDiv.style.display = "block";
+}
+//score render
+function scoreRender() {
+	scoreDiv.style.display = "block";
 
-		//calculate the amount of the question percent answered by the user
-		const scorePerCent= Math.round(100 * socre/questions.length);
-		//choosethe image based on the scorePerCent
-		let img = (scorePerCent >= 80) ? "img/5.png":
-		(scorePercent >= 60) ? "img/4.png":
-		(scorePercent >= 40) ? "img/3.png":	
-		(scorePercent >= 20) ? "img/2.png":
-		"img/1.png";
-		score.Div.innerHTMl = "<img src="+ ">";
-		scorediv.innerHTML += "<p>" + scorePerCent + "%</p>";
-	
-	
-	
-	
-	}
+	//calculate the amount of the question percent answered by the user
+	const scorePerCent = Math.round(100 * socre / questions.length);
+	//choose the image based on the scorePerCent
+	let img = (scorePerCent >= 80) ? "img/5.png" :
+		(scorePercent >= 60) ? "img/4.png" :
+			(scorePercent >= 40) ? "img/3.png" :
+				(scorePercent >= 20) ? "img/2.png" :
+					"img/1.png";
+	score.Div.innerHTMl = "<img src=" + ">";
+	scorediv.innerHTML += "<p>" + scorePerCent + "%</p>";
 
 
- 
+
+
+}
+
+
+
 
 
 
