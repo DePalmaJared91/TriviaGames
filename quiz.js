5e314e6d76a9a37858c9e3f71d4e1e23399b5dc3
Jared DePalma <depalmajared91@gmail.com>
2020-03-05 01:11:01 -0700

    Put another 4+ hours into this after the intitial 2.5 I did earlier today. Sitll trying to make it better but as of now I have a semi-working app.

--- quiz.js
+++ quiz.js
@@ -13,8 +13,8 @@ const scoreDiv = document.getElementById("scoreDiv");
 // question & answer array
 let questions = [
 	{
-		question: "Originally named The Collegiate Shcool, Yale University took on its new name in what year?",
-		imgSrc: "images/collegiateschool.jpeg",
+		question: "Originally named, The Collegiate School, Yale University took on its new name in what year?",
+		imgSrc: "../img/collegiateschool.jpeg",
 		choiceA: "1770",
 		choiceB: "1778",
 		choiceC: "1701",
@@ -22,117 +22,185 @@ let questions = [
 	},
 	{
 		question: "The Yale Daily News is the country's oldest collegiate newspaper which has been in print since. . . ?",
-		imgSrc: "images/YDN.jpeg",
+		imgSrc: "../img/YDN.jpeg",
 		choiceA: "1922",
 		choiceB: "1791",
 		choiceC: "1878",
 		correct: "C"
-	}];
+	},
+	{
+		question: "Nine of Yale's ten founders taught at ______ University, because their school stopped requiring freshmen to study biblical Hebrew?",
+		imgSrc: "..//img/Harvard.jpg",
+		choiceA: "Harvard",
+		choiceB: "Princeton",
+		choiceC: "Brown",
+		correct: "A"
+	},
+
+
+];
+
+
 
 //:variables
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-const questionTime = 10; //10s
+let lastQuestionIndex = questions.length - 1; 
+let runningQuestionIndex = 0;
+
+const questionTime = 10; //15s
 const gaugeWidth = 150;//150px
-const gaugeUnit = gaugeWidth / questionTime;
+let count = 0;
+const gaugeProgressUnit = gaugeWidth / questionTime;
+
 let TIMER;
 let score = 0;
 
+//start quiz
+start.addEventListener("click", startQuiz);
+
+function startQuiz() {
+
+	start.style.display = "none";
+	counterRender();
+	
+	renderQuestion();
+	startQuiz.style.display = display;
+	progressRender();
+	
+	TIMER = setInterval(counterRender, 1000);
+}
+
+
+
+
+
+
+
+
+
 //render a question
 function renderQuestion() {
-	let q = questions[runningQuestion];
-
-	question.innerHTML = "<p>" + q.question + "</p>";
+	let q = questions[runningQuestionIndex];
 	qImg.innerHTML = "<img src" + q.imgSrc + ">";
+	question.innerHTML = "<p>" + q.question + "</p>";
+	
 	choiceA.innerHTML = q.choiceA;
 	choiceB.innerHTML = q.choiceB;
 	choiceC.innerHTML = q.choiceC;
 }
-start.addEventListener("click", startQuiz);
-//start quiz
-function startQuiz() {
+ runningQuestionIndex = 0;
+renderQuestion();
+runningQuestionIndex++;
+renderQuestion;
 
-	start.style.display = "none";
-	renderQuestion();
-	quiz.style.display = "block";
-	renderProgress();
-	renderCounter();
-	TIMER = setInterval(renderCounter, 1000);
-}
-//render progress
-function renderProgress() {
-	for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+
+
+//progress Render
+function progressRender() {
+	for (let qIndex = 0; qIndex <= lastQuestionIndex; qIndex++) {
 
 		progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
 
 	}
 }
+function answerIsCorrect() {
+	document.getElementById(runningQuestionIndex).style.backgroundColor = "green";
+}
+//answer is wrong
+function answerIsWrong() {
+	document.getElementById(runningQuestionIndex).style.backgroundColor = "red";
+}
+
+///
+
+
+
+
+
+
 
 //counter render
 
-function renderCounter() {
+
+
+
+const gaugeProgressUnit = gaugeWidth / questionTime;
+
+
+
+
+
+
+
+
+function counterRender() {
 	if (count <= questionTime) {
 		counter.innerHTML = count;
-		timeGauge.style.width = count * gaugeUnit + "px";
-		count++
+		timeGauge.style.width = gaugeProgressUnit * count + "px";
+		count++;
 
 	} else {
 		count = 0
 		//change progress color to red
 		answerIsWrong();
-		if (runningQuestion < lastQuestion) {
-			runningQuestion++;
-			renderQuestion();
-
-		} else {//end quiz show score
-			clearInterval(TIMER);
+		if (runningQuestionIndex < lastQuestionIndex) {
+			runningQuestionIndex++;
+			questionRender();
+//end quiz show score
+		} else {  clearInterval(TIMER);
 			scoreRender();
 
 		}
 	}
 }
-//pick up here
+let TIMER =
+setInterval(counterRender,1000);
+clearInterval();
+
+
 
 
 //check answer
-function checkAnswer(answer){
-	if(answer == questions[runningQuestion].correct){
-	//answer is correct	
-	score++;
-	// change progress color to red
-	answerIsCorrect();
-}else {
-	//answer is wrong
-	//change progress color to red
-	answerIsWrong();
+let score = 0;
+function checkAnswer(answer) {
+	if(questions[runningQuestionIndex].correct == answer) {
+		//answer is correct	
+		score++;
+		// change progress color to red
+		answerIsCorrect();
+	} else {
+		//answer is wrong
+		//change progress color to red
+		answerIsWrong();
+	}
+	
+	if (runningQuestionIndex < lastQuestionIndex) {
+		count=0
+		runningQuestion++;
+		questionRender();
+	} else {
+		//end quiz show score
+		clearInterval(TIMER);
+		scoreRender();
+
+	}
 }
-count = 0
-if (runningQuestion < lastQuestion) {
-	runningQuestion++;
-	renderQuestion();
-} else {
-	//end quiz show score
-	clearInterval(TIMER);
-	scoreRender();
-
-}}
-
+//These lines below may need tyo get deleted ....
 //answer is correct
-function answerIsCorrect() {
-	document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-}
-//answer is wrong
-function answerIswrong() {
-	document.getElementById(runningQuestion).style.backgroundColor = "f00";
+//function answerIsCorrect() {
+	//document.getElementById(runningQuestionIndex).style.backgroundColor = "#0f0";
 
-}
+//answer is wrong
+//function answerIswrong() {
+	//document.getElementById(runningQuestionIndex).style.backgroundColor = "f00";
+
+//}
+//LInes ABove may be deleted.....
 //score render
 function scoreRender() {
 	scoreDiv.style.display = "block";
 
 	//calculate the amount of the question percent answered by the user
-	const scorePerCent = Math.round(100 * socre / questions.length);
+	const scorePerCent = Math.round(100 * score / questions.length);
 	//choose the image based on the scorePerCent
 	let img = (scorePerCent >= 80) ? "img/5.png" :
 		(scorePercent >= 60) ? "img/4.png" :
